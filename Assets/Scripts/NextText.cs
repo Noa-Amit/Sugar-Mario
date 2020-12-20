dd83522e6605cae0940dc63fb79766feb2f5f808
noayair <noashefer5@gmail.com>
2020-12-20 17:51:28 +0200

    Tutorial

---
+++ Assets/Scripts/NextText.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using System;
+using UnityEngine.Events;
+
+public class NextText : MonoBehaviour, IPointerClickHandler, IPointerDownHandler, IPointerUpHandler
+{
+
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log("hey");
+      //  MyOwnEventTriggered();
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+    }
+
+
+    //my event
+    [Serializable]
+    public class MyOwnEvent : UnityEvent { }
+
+    [SerializeField]
+    private MyOwnEvent myOwnEvent = new MyOwnEvent();
+    public MyOwnEvent onMyOwnEvent { get { return myOwnEvent; } set { myOwnEvent = value; } }
+
+    public void MyOwnEventTriggered()
+    {
+        onMyOwnEvent.Invoke();
+    }
+}
