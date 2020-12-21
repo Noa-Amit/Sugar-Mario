a8acdbd4a7dba1ca653e389e7b0199a80ce4674c
noayair <noashefer5@gmail.com>
2020-12-21 15:16:33 +0200

    text and bold

--- Assets/Scripts/NextText.cs
+++ Assets/Scripts/NextText.cs
@@ -1,15 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
-using System;
-using UnityEngine.Events;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class NextText : MonoBehaviour
 {
+    private GameObject bubble;
+    static int textNum;
+    GameObject currentText;
+    GameObject currentBold;
+
+    void Start()
+    {
+        textNum = 1;
+        bubble = gameObject.transform.root.gameObject;
+        string name = "Text" + (textNum++);
+        string boldName = "Bold" + (textNum);
+        currentText = bubble.transform.Find(name).gameObject;
+        currentBold = bubble.transform.Find(boldName).gameObject;
+    }
     public void Test()
     {
-        Debug.Log("ok");
+        Debug.Log(currentText.name);
+        Debug.Log(currentBold.name);
+        currentText.SetActive(false);
+        currentBold.SetActive(false);
+        string name = "Text" + (textNum++);
+        string boldName = "Bold" + (textNum);
+        Debug.Log(name);
+        Debug.Log(boldName);
+        if (textNum <= 11)
+        {
+            currentText = bubble.transform.Find(name).gameObject;
+            currentBold = bubble.transform.Find(boldName).gameObject;
+            currentText.SetActive(true);
+            currentBold.SetActive(true);
+        }
+        else
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+
     }
 }
