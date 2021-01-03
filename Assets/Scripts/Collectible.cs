86276711cf2886e8ef05a9259a821a6e1c6d5995
Amit Ran <57709369+amitRan109@users.noreply.github.com>
2021-01-03 20:58:03 +0200

    add floating text

--- Assets/Scripts/Collectible.cs
+++ Assets/Scripts/Collectible.cs
@@ -1,13 +1,21 @@
 ﻿using UnityEngine;
+using TMPro;
 
 public class Collectible : MonoBehaviour{
     [SerializeField] string triggeringTag;
     [SerializeField] double changeSugarOnTrigger;
+    public GameObject floatingText;
 
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.tag == triggeringTag) {
         	other.gameObject.GetComponent<FieldsChanger>().addToSugar(changeSugarOnTrigger);
             Destroy(gameObject);
+            if(floatingText) ShowFloatingText();
         }
+    }
+
+    private void ShowFloatingText(){
+    	var go =Instantiate(floatingText, transform.position, Quaternion.identity, transform);
+    	go.GetComponent<TextMeshPro>().text = changeSugarOnTrigger.ToString();
     }
 }
